f84cf71b9dfc4b330934148afb1dcda6d4fbd5c1
sun-sunDayz <kill.yanggochi@gmail.com>
2024-05-14 15:47:53 +0900

    Account 모델 추가

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    # login email 로 ~ 
+    email = models.EmailField(unique=True)
+    introduce = models.TextField(blank=True)
+    following = models.ManyToManyField("self", symmetrical=False, through="Follow", related_name="followers")
+    
+    percentIE = models.FloatField(default=0)
+    percentNS = models.FloatField(default=0)
+    percentFT = models.FloatField(default=0)
+    percentPJ = models.FloatField(default=0)
+    
