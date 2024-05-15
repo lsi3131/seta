72bc6a872f448be23598155110e0adbd2f466741
sang-il <lsi9393@naver.com>
2024-05-15 16:01:28 +0900

    mbti 테스트 확인 후 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg'
 import './App.css'
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import Cursor from './Components/Cursor/Cursor'
+import CommentBox from "./Components/Comment/Comment";
+import NavBar from "./Components/NavBar/NavBar";
+
+const mbti_list = [
+    'INFP', 'ISFP', 'ENFP', 'ESFP'
+]
 
 function App() {
     return (
         <>
-            <Cursor />
+            <Cursor/>
+            <NavBar/>
+            <CommentBox/>
+            {mbti_list.map((mbti) => (
+                <>
+                    <div>
+                        <h1>helloworld-{mbti}</h1>
+                    </div>
+                </>
+            ))}
         </>
     )
 }
---
+++ frontend/src/Components/Board/Board.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Comment.module.css'
+
+const Board = ({mbti}) => {
+    return (
+        <>
+            <div>
+                <h1>Board-{mbti}</h1>
+            </div>
+        </>
+    )
+}
+
+export default Board;
+
