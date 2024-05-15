fb4b5380c8321b697c287d6783b6fab92b21df88
sang-il <lsi9393@naver.com>
2024-05-15 14:06:11 +0900

    feat 회원가입 유효성 CRUD * 이름, 비밀번호, 이메일 유효성 검사

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -27,7 +27,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/account/", include("account.urls")),
+    path("api/accounts/", include("account.urls")),
     path("api/posts/", include("post.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
