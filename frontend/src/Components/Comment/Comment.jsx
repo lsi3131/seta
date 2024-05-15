e24b185999abe1026c693bf063b9a9ace6899689
sang-il <lsi9393@naver.com>
2024-05-15 16:01:28 +0900

    mbti 테스트 확인 후 commit

---
+++ frontend/src/Components/Comment/Comment.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Comment.module.css'
+
+const CommentBox = () => {
+    return (
+        <div>
+            <h1>helloworld</h1>
+            <button className={style.button}>helloworld</button>
+        </div>
+    )
+}
+
+export default CommentBox;
+
---
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import style from './NavBar.module.css'
+
+const AuthenticatedNavbar = ({ username }) => {
+    return (
+        <nav className={style.navbar}>
+            <div className="left">
+                <Link to="/">HOME</Link>
+                <Link to="/ask">ASK</Link>
+                <Link to="/show">SHOW</Link>
+                <Link to="/write">WRITE</Link>
+            </div>
+            <div className="right">
+                <Link to={`/profile/${username}/`}>{username}</Link>
+            </div>
+        </nav>
+    )
+}
+
+// 인증되지 않은 사용자를 위한 네비게이션
+const UnauthenticatedNavbar = () => {
+    return (
+        <nav className={style.navbar}>
+            <div className="left">
+                <Link to="/">HOME</Link>
+                <Link to="/ask">ASK</Link>
+                <Link to="/show">SHOW</Link>
+                <Link to="/write">WRITE</Link>
+            </div>
+            <div className="right">
+                <Link to="/login">LOGIN</Link>
+            </div>
+        </nav>
+    )
+}
+const Navbar = ({ username }) => {
+    return <div>{username ? <AuthenticatedNavbar username={username} /> : <UnauthenticatedNavbar />}</div>
+}
+export default Navbar
