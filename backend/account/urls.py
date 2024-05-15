a3c838faf25b131a96d9fcd5dadbfb32f4b20e52
sang-il <lsi9393@naver.com>
2024-05-15 16:45:21 +0900

    merge from main

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.AccountAPIView.as_view()),
     path('validate/password/', views.validate_password),
     path('validate/username/', views.validate_username),
     path('validate/email/', views.validate_email),
