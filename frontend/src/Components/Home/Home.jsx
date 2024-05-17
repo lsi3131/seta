418e5665826a930f2b80b57e2e41a05b0c0a25e9
LeeJS9856 <wnstj1057@naver.com>
2024-05-17 20:47:16 +0900

    feat : navbar 임시추가 및 Carousel 추가

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,11 +1,26 @@
 import style from "./Home.module.css";
 import React from "react";
 import BoardCardList from "../BoardCard/BoardCard";
+import ImageSlider from "../ImageSlider/ImageSlider";
 
 const Home = () => {
+    const slides = [
+        { url: "http://via.placeholder.com/1200x501", title: "image1"},
+        { url: "http://via.placeholder.com/1200x502", title: "image2"},
+        { url: "http://via.placeholder.com/1200x503", title: "image3"},
+        { url: "http://via.placeholder.com/1200x504", title: "image4"}
+    ];
+
+    const containerStyles = {
+        width: "1200px",
+        height: "500px",
+        margin: "0 auto"
+    }
     return (
         <div>
-            <h1>상단 회색 박스</h1>
+            <div style={containerStyles}>
+                <ImageSlider slides={slides} />
+            </div>
 
             <h1>MBTI 게시판</h1>
             <BoardCardList/>
