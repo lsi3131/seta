10e8ca12865d27273fa59ad9780706741b6b88d8
LeeJS9856 <wnstj1057@naver.com>
2024-05-20 01:00:01 +0900

    BoardCard 추가 (mbti 사진 X)

--- frontend/src/Components/BoardCard/BoardCard.jsx
+++ frontend/src/Components/BoardCard/BoardCard.jsx
@@ -4,16 +4,31 @@ import {Link} from "react-router-dom";
 
 
 const mbti_list = [
-    'INTJ', 'ENTP', 'ENTJ', 'ENTP',
-    'INFJ', 'INFP', 'ENFJ', 'ENFP',
-    'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ',
-    'ISTP', 'ISFP', 'ESTP', 'ESFP',
+    { url: "http://via.placeholder.com/250x250", mbti: "INTJ"},
+    { url: "http://via.placeholder.com/250x250", mbti: "ENTP"},
+    { url: "http://via.placeholder.com/250x250", mbti: "ENTJ"},
+    { url: "http://via.placeholder.com/250x250", mbti: "ENTP"},
+    { url: "http://via.placeholder.com/250x250", mbti: "INFJ"},
+    { url: "http://via.placeholder.com/250x250", mbti: "INFP"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ENFJ"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ENFP"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ISTJ"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ISFJ"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ESTJ"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ESFJ"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ISTP"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ISFP"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ESTP"}, 
+    { url: "http://via.placeholder.com/250x250", mbti: "ESFP"}, 
 ]
+
 const BoardCard = ({mbti}) => {
     return (
         <>
             <div className={style.div}>
-                <Link to={`/board/${mbti}/`}><h3>{mbti}</h3></Link>
+                <Link to={`/board/${mbti.mbti}/`}>
+                    <img src={mbti.url} alt={mbti.mbti} className={style.image}/>
+                </Link>
             </div>
         </>
     )
@@ -21,13 +36,15 @@ const BoardCard = ({mbti}) => {
 
 const BoardCardList = () => {
     return (
-        <>
-            {mbti_list.map((mbti) => (
-                <>
-                    <BoardCard mbti={mbti}/>
-                </>
-            ))}
-        </>
+        <div className={style.container}>
+            <div className={style.cardList}>
+                {mbti_list.map((mbti) => (
+                    <>
+                        <BoardCard mbti={mbti}/>
+                    </>
+                ))}
+            </div>
+        </div>
     )
 }
 
