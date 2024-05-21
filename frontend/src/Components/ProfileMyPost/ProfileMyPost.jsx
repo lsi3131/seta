3c9b419bd37d8167e3b70b1daea9087cdb44c204
김병민 <gimbyeongmin@byeong.local>
2024-05-22 08:55:37 +0900

    내가 작성한 게시물과 좋아요한 게시물 기능 구현

--- frontend/src/Components/ProfileMyPost/ProfileMyPost.jsx
+++ frontend/src/Components/ProfileMyPost/ProfileMyPost.jsx
@@ -1,35 +1,59 @@
-import React, { useContext, useEffect, useState } from "react";
-import { UserContext } from "userContext";
+import React, { useEffect, useState, useContext } from "react";
 import style from "./ProfileMyPost.module.css"
 import apiClient from "services/apiClient";
 import { Link } from "react-router-dom";
 import { formatDate, getFontColor, getButtonColor } from "../../Utils/helpers"
-
+import Pagination from '../Pagenation/Pagination'
+import { UserContext } from "userContext";
 
 
 
 const ProfileMyPost = (users) => {
     const [view, setView] = useState('posts')
+    const [posts, setPosts] = useState()
+    const [error, setError] = useState(null)
+    const [currentPage, setCurrentPage] = useState(1);
+    const currentUser = useContext(UserContext)
     const user = users.props
 
-    const [visivlePosts, setVisivlePosts] = useState(5)
 
+    useEffect(() => {
+        if (!user || !user.username) {
+            return;
+        }
 
-    const MorePosts = () => {
-        setVisivlePosts((prevVisiblePosts) => prevVisiblePosts + 5)
+        async function fetchData(page = 1) {
+            try{
+                const response = await apiClient.get(`api/accounts/${user.username}/myposts/?page=${page}`)
+                setPosts({
+                    posts: response.data.paginated_posts,
+                    like_posts: response.data.paginated_like_posts
+                    })
+            } catch (error) {
+                setError('데이터를 불러오는데 실패했습니다.')
+            }
+        }
+        fetchData(currentPage);
+    }, [user ,currentPage]);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    if(!posts){  
+        return <div></div>
     }
-
-    console.log(user)
     return (
         <div className={style.board_contents}>
             <div className={style.board_category}>
                 <button onClick={() => setView('posts')}>내가 작성한 글</button>
-                <button onClick={() => setView('like_posts')}>좋아요 한 글 </button>
+                {user.usernam===currentUser ? 
+                <button onClick={() => setView('like_posts')}>좋아요 한 글 </button> : null}
             </div>
             <hr />
-            {(view === 'posts' || view === 'like_posts') && user.posts && (
-                <div key={user.id} className={style.board_posts}>
-                    {(view === 'posts' ? user.posts : user.like_posts).map((post) => (
+            {(view === 'posts' || view === 'like_posts') && (
+                <div key={posts.id} className={style.board_posts}>
+                    {(view === 'posts' ? posts.posts.results : posts.like_posts.results).map((post) => (
                         <>
                             <div key={post.id} className={style.board_post}>
                                 <div className={style.board_post_left}>
@@ -52,8 +76,8 @@ const ProfileMyPost = (users) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div className={style.board_post_right}>
-                                    <div >
+                                <div>
+                                    <div className={style.board_post_right}>
                                         {post.mbti.map(m => (
                                             <p style={{ backgroundColor: getButtonColor(m) }}>{m}</p>
                                         ))}
@@ -65,6 +89,11 @@ const ProfileMyPost = (users) => {
                     ))}
                 </div>
             )}
+            <Pagination 
+            currentPage={currentPage}
+            totalPages={(view === 'posts' ? posts.posts.total_page : posts.like_posts.total_page)}
+            onPageChange={handlePageChange}
+            />
         </div>
     )
 }
