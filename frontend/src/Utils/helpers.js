b9553699fba8cc3fdc351796ef12d4c12c6caba2
김병민 <gimbyeongmin@byeong.local>
2024-05-21 17:34:15 +0900

    내가 좋아요 한글, 내가작성한글 디자인 변경

--- frontend/src/Utils/helpers.js
+++ frontend/src/Utils/helpers.js
@@ -126,7 +126,7 @@ export const getMainColor = (mbti) => {
     }
     if (!mbti) return null;
 
-    return mbtiParams[mbti].mainColor
+    return mbtiParams[mbti.toLowerCase()].mainColor
 }
 export const getFontColor = (mbti) => {
     if (mbti === null) {
