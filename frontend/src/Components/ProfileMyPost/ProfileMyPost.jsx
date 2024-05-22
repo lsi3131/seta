08558f26ad8d10004eefc0fde6fcf0065c5307f0
김병민 <gimbyeongmin@byeong.local>
2024-05-22 15:10:47 +0900

    좋아요글, 작성한 글

--- frontend/src/Components/ProfileMyPost/ProfileMyPost.jsx
+++ frontend/src/Components/ProfileMyPost/ProfileMyPost.jsx
@@ -16,43 +16,52 @@ const ProfileMyPost = (users) => {
     const currentUser = useContext(UserContext)
     const user = users.props
 
-
     useEffect(() => {
         if (!user || !user.username) {
             return;
         }
 
         async function fetchData(page = 1) {
-            try{
+            try {
                 const response = await apiClient.get(`api/accounts/${user.username}/myposts/?page=${page}`)
-                setPosts({
-                    posts: response.data.paginated_posts,
-                    like_posts: response.data.paginated_like_posts
+
+                if (response.data.paginated_like_posts) {
+                    setPosts({
+                        posts: response.data.paginated_posts,
+                        like_posts: response.data.paginated_like_posts
                     })
+                } else {
+                    setPosts({
+                        posts: response.data.paginated_posts,
+                        like_posts: null
+                    })
+                }
+
             } catch (error) {
                 setError('데이터를 불러오는데 실패했습니다.')
             }
         }
         fetchData(currentPage);
-    }, [user ,currentPage]);
+    }, [user, currentPage]);
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
     };
 
-    if(!posts){  
+    if (!posts) {
         return <div></div>
     }
-    
+
     return (
         <div className={style.board_contents}>
             <div className={style.board_category}>
                 <button onClick={() => setView('posts')}>내가 작성한 글</button>
-                {user.usernam===currentUser ? 
-                <button onClick={() => setView('like_posts')}>좋아요 한 글 </button> : null}
+                {user.username === currentUser.username && (
+                    <button onClick={() => setView('like_posts')}>좋아요 한 글</button>
+                )}
             </div>
             <hr />
-            {(view === 'posts' || view === 'like_posts') && (
+            {(view === 'posts' || (view === 'like_posts' && posts.like_posts !== null)) && (
                 <div key={posts.id} className={style.board_posts}>
                     {(view === 'posts' ? posts.posts.results : posts.like_posts.results).map((post) => (
                         <>
@@ -77,10 +86,10 @@ const ProfileMyPost = (users) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div>
+                                <div >
                                     <div className={style.board_post_right}>
                                         {post.mbti.map(m => (
-                                            <p style={{ backgroundColor: getButtonColor(m) }}>{m}</p>
+                                            <button style={{ backgroundColor: getButtonColor(m) }}>{m}</button>
                                         ))}
                                     </div>
                                 </div>
@@ -90,10 +99,10 @@ const ProfileMyPost = (users) => {
                     ))}
                 </div>
             )}
-            <Pagination 
-            currentPage={currentPage}
-            totalPages={(view === 'posts' ? posts.posts.total_page : posts.like_posts.total_page)}
-            onPageChange={handlePageChange}
+            <Pagination
+                currentPage={currentPage}
+                totalPages={(view === 'posts' ? posts.posts.total_page : posts.like_posts !== null ? posts.like_posts.total_page : 0)}
+                onPageChange={handlePageChange}
             />
         </div>
     )
