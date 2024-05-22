f04351b3f9d21af1e1b339e8f2997816808b1722
sang-il <lsi9393@naver.com>
2024-05-22 09:33:46 +0900

    중간 상태 저장

--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -8,6 +8,7 @@ import apiClient from "services/apiClient";
 import { UserContext } from "userContext";
 import {formatDate, mbtiParams, getImage, getFontColor, getButtonColor} from "../../Utils/helpers"
 import ProfileMyPost from "Components/ProfileMyPost/ProfileMyPost";
+import ProfileTop from "./ProfileTop/ProfileTop";
 
 
 
@@ -33,49 +34,7 @@ const Profile = () => {
 
     return (
         <div className={style.vertical}>
-            <div className={style.board_top} >
-                <div className={style.board_top_content}>
-                    <div className={style.board_top_lift}>
-                        <div className={style.board_top_text_container}>
-                            <p>{users.mbti}</p>
-                            <h2>{users.username}</h2>
-                        </div>
-                        <div>
-                            <img className={style.board_top_image} src={getImage(users.mbti)} />
-                        </div>
-                    </div>
-                    <div className={style.board_top_right}>
-                        <div className={style.board_top_count}>
-                            <div>
-                                {users.posts && <h2>{users.posts.length}</h2>}
-                                <p>게시물</p>
-                            </div>
-                            <div>
-                                <h2>{users.followers_count}</h2>
-                                <p>팔로워</p>
-                            </div>
-                            <div>
-                                <h2>{users.following_count}</h2>
-                                <p>팔로잉</p>
-                            </div>
-                        </div>
-                        <div className={style.boder_top_mbti_box}>
-                            {users.mbti &&
-                                <div className={style.board_top_mbti}>
-                                    <h3>{users.mbti[0]}</h3>
-                                    <p>{Math.floor(users.percentIE)}%</p>
-                                    <h3>{users.mbti[1]}</h3>
-                                    <p>{Math.floor(users.percentNS)}%</p>
-                                    <h3>{users.mbti[2]}</h3>
-                                    <p>{Math.floor(users.percentFT)}%</p>
-                                    <h3>{users.mbti[3]}</h3>
-                                    <p>{Math.floor(users.percentPJ)}%</p>
-                                </div>
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <ProfileTop users={users}/>
             <ProfileMBTIForm />
             <ProfileMyPost props={users}/>
         </div>
