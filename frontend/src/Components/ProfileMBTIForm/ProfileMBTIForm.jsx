4e62d8ddb274cddee27d056d0716b307a861ba93
sang-il <lsi9393@naver.com>
2024-05-22 14:45:46 +0900

    feat 팔로잉, 팔로워 랭킹 프론트작업 * 차트에 팔로잉, 팔로워 랭킹 적용

--- frontend/src/Components/ProfileMBTIForm/ProfileMBTIForm.jsx
+++ frontend/src/Components/ProfileMBTIForm/ProfileMBTIForm.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import style from "./ProfileMBTIForm.module.css";
-import { ResponsiveBar } from '@nivo/bar'
+import {ResponsiveBar} from '@nivo/bar'
+import apiClient from "../../services/apiClient";
 
 const data = [
     {
@@ -28,14 +29,11 @@ const data = [
 const entjFormat = (value) => {
     if (value === 'EI') {
         return 'E';
-    }
-    else if (value === 'NS') {
+    } else if (value === 'NS') {
         return 'N';
-    }
-    else if (value === 'TF') {
+    } else if (value === 'TF') {
         return 'T';
-    }
-    else if (value === 'JP') {
+    } else if (value === 'JP') {
         return 'J';
     }
 }
@@ -43,20 +41,17 @@ const entjFormat = (value) => {
 const isfpFormat = (value) => {
     if (value === 'EI') {
         return 'I'
-    }
-    else if (value === 'NS') {
+    } else if (value === 'NS') {
         return 'S';
-    }
-    else if (value === 'TF') {
+    } else if (value === 'TF') {
         return 'F';
-    }
-    else if (value === 'JP') {
+    } else if (value === 'JP') {
         return 'P';
     }
 }
 
 
-const ProfileChart = ({ data }) => (
+const ProfileChart = ({data}) => (
     <ResponsiveBar
         data={data}
         keys={[
@@ -74,13 +69,13 @@ const ProfileChart = ({ data }) => (
             fontSize: '20px',
         }}
         padding={0.3}
-        margin={{right: 20, left: 20 }}
+        margin={{right: 20, left: 20}}
         layout="horizontal"
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
+        valueScale={{type: 'linear'}}
+        indexScale={{type: 'band', round: true}}
         colors={['#a9a9a9', '#d9d9d9']}
         borderWidth={2}
-        borderColor={{ theme: 'background' }}
+        borderColor={{theme: 'background'}}
         axisTop={null}
         axisRight={{
             tickSize: 0,
@@ -107,11 +102,11 @@ const ProfileChart = ({ data }) => (
         isInteractive={false}
         role="application"
         ariaLabel="Nivo bar chart demo"
-        barAriaLabel={e=>e.id+": "+e.formattedValue+" in mbti: "+e.indexValue}
+        barAriaLabel={e => e.id + ": " + e.formattedValue + " in mbti: " + e.indexValue}
     />
 )
 
-const ProfileMBTIForm = () => {
+const ProfileMBTIForm = ({followingRanks, followerRanks}) => {
     const introduce = `난 너를 믿었던만큼 난 내 친구도 믿었기에
     난 아무런 부담없이 널 내 친구에게 소개시켜 줬고
     그런 만남이 있은 후부터 우리는 자주 함께 만나며
@@ -122,12 +117,29 @@ const ProfileMBTIForm = () => {
     너와 내 친구는 연락도 없고 날 피하는 것 같아
     그제서야 난 느낀거야 모든 것이 잘못돼 있는걸
     너와 내 친구는 어느새 다정한 연인이 돼 있었지`
-    
+
+    useEffect(() => {
+
+    }, [followingRanks, followerRanks])
+
+    const getRankText = (ranks, rankNum /* 1~3*/) => {
+        /*
+            ranks = [('ISFP', 3), ('ENFP', 2), ('ENTP, 5)]
+         */
+        const rankIndex = rankNum - 1;
+        if(ranks.length <= rankIndex || 0 > rankIndex) {
+            return "아직은 없어요";
+        }
+
+        return ranks[rankIndex][0].toUpperCase();
+    }
+
+
     return (
-        <div style={{ display:"flex", justifyContent: "space-around" }}>
-            <div >
+        <div style={{display: "flex", justifyContent: "space-around"}}>
+            <div>
                 <div className={style.container}>
-                    <h3 className={style.title}>자기소개</h3>   
+                    <h3 className={style.title}>자기소개</h3>
                     <div className={style.introduce}>
                         <h4>{introduce}</h4>
                     </div>
@@ -144,17 +156,17 @@ const ProfileMBTIForm = () => {
                 <div className={style.container}>
                     <h3 className={style.title}>내가 팔로우한 mbti 랭킹</h3>
                     <div className={style.followRanking}>
-                    <div className={style.rankingContanier}>
-                            <h3 className={style.rankingMbtiText}>ISFJ</h3>
-                            <div className={style.second} style={{ '--target-height': '70px' }}/>
+                        <div className={style.rankingContanier}>
+                            <h3 className={style.rankingMbtiText}>{getRankText(followingRanks, 2)}</h3>
+                            <div className={style.second} style={{'--target-height': '70px'}}/>
                         </div>
                         <div className={style.rankingContanier}>
-                            <h3 className={style.rankingMbtiText}>ESFJ</h3>
-                            <div className={style.first} style={{ '--target-height': '120px' }}/>
+                            <h3 className={style.rankingMbtiText}>{getRankText(followingRanks, 1)}</h3>
+                            <div className={style.first} style={{'--target-height': '120px'}}/>
                         </div>
-                        <div className={style.rankingContanier}>
-                            <h3 className={style.rankingMbtiText}>INTP</h3>
-                            <div className={style.third} style={{ '--target-height': '40px' }}/>
+                        <div classname={style.rankingcontanier}>
+                            <h3 className={style.rankingMbtiText}>{getRankText(followingRanks, 3)}</h3>
+                            <div className={style.third} style={{'--target-height': '40px'}}/>
                         </div>
                     </div>
                 </div>
@@ -162,16 +174,16 @@ const ProfileMBTIForm = () => {
                     <h3 className={style.title}>나를 팔로우한 mbti 랭킹</h3>
                     <div className={style.followerRanking}>
                         <div className={style.rankingContanier}>
-                            <h3 className={style.rankingMbtiText}>ISFJ</h3>
-                            <div className={style.second} style={{ '--target-height': '70px' }}/>
+                            <h3 className={style.rankingMbtiText}>{getRankText(followerRanks, 2)}</h3>
+                            <div className={style.second} style={{'--target-height': '70px'}}/>
                         </div>
                         <div className={style.rankingContanier}>
-                            <h3 className={style.rankingMbtiText}>ESFJ</h3>
-                            <div className={style.first} style={{ '--target-height': '120px' }}/>
+                            <h3 className={style.rankingMbtiText}>{getRankText(followerRanks, 1)}</h3>
+                            <div className={style.first} style={{'--target-height': '120px'}}/>
                         </div>
                         <div className={style.rankingContanier}>
-                            <h3 className={style.rankingMbtiText}>INTP</h3>
-                            <div className={style.third} style={{ '--target-height': '40px' }}/>
+                            <h3 className={style.rankingMbtiText}>{getRankText(followerRanks, 3)}</h3>
+                            <div className={style.third} style={{'--target-height': '40px'}}/>
                         </div>
                     </div>
                 </div>
