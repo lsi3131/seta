4906ec9396783bf0dd317513c505e42127357587
sang-il <lsi9393@naver.com>
2024-05-27 19:59:40 +0900

    feat 검색 기능 * 검색 기능 1차 구현 완료

--- frontend/src/Components/Search/SearchResult.jsx
+++ frontend/src/Components/Search/SearchResult.jsx
@@ -2,36 +2,18 @@ import React, {useEffect, useState} from "react";
 import {Link, useLocation, useNavigate} from "react-router-dom";
 import style from "./SearchResult.module.css"
 import apiClient from "../../services/apiClient";
+import SearchPost from "./SearchPost";
+import Pagination from "../Pagenation/Pagination";
 
-const CommentPostList = ({comments}) => {
 
-}
-
-const SearchPost = ({post}) => {
-    const navigate = useNavigate()
-
-    const moveToPost = (postMbti) => {
-        navigate(`/board/${postMbti}/`)
-    }
-
-    return (
-        <div className={style.search_post_container}>
-            <p>[{post.author_mbti}]</p>
-            <Link to={`/detail/${post.id}?mbti=${post.author_mbti}`}>{post.title}</Link>
-            <h3>{`/detail/${post.id}?mbti=${post.author_mbti}`}</h3>
-            <p>{post.content}</p>
-        </div>
-    )
-}
-
-const SearchPostList = ({posts}) => {
+const SearchPostList = ({posts, searchCount}) => {
     useEffect(() => {
         //post 변경에 다른 값 갱신
     }, [posts])
 
     return (
-        <div className={style.search_post_list_container}>
-            <h2>게시글 검색 결과 {posts.length}건</h2>
+        <div className={style.list_container}>
+            <h2>게시글 검색 결과 {searchCount}건</h2>
             {posts.map((post) => (
                 <>
                     <SearchPost post={post}/>
@@ -48,6 +30,9 @@ const SearchResult = ({}) => {
     const [searchPosts, setSearchPosts] = useState([]);
     const [category, setCategory] = useState('');
     const [order, setOrder] = useState('');
+    const [totalPage, setTotalPage] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [searchCount, setSearchCount] = useState(0)
 
     useEffect(() => {
         handlePageChange(1)
@@ -66,9 +51,11 @@ const SearchResult = ({}) => {
         apiClient
             .get(url)
             .then((response) => {
+                console.log(response.data)
                 setSearchPosts(response.data['results'])
-                // setTotalPage(response.data['total_page'])
-                // setCurrentPage(page)
+                setSearchCount(response.data['count'])
+                setTotalPage(response.data['total_page'])
+                setCurrentPage(page)
             })
             .catch((error) => {
                 console.error('Error during get posts:', error)
@@ -78,11 +65,12 @@ const SearchResult = ({}) => {
 
     return (
         <>
-            <SearchPostList posts={searchPosts}/>
-            <hr/>
+            <div className={style.container}>
+                <SearchPostList posts={searchPosts} searchCount={searchCount}/>
 
-            {/*<h2>댓글 검색 결과 {searchPosts.length}건</h2>*/}
-            {/*<hr/>*/}
+                {/*<h2>댓글 검색 결과 {searchPosts.length}건</h2>*/}
+                <Pagination currentPage={currentPage} totalPages={totalPage} onPageChange={handlePageChange}/>
+            </div>
 
         </>
     )
