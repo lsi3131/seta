f51b43aaf02a4a3c04e5833a024401ec3468d798
김병민 <gimbyeongmin@byeong.local>
2024-05-27 16:03:25 +0900

    이메일 인증

--- backend/config/serializers.py
+++ backend/config/serializers.py
@@ -1,19 +1,37 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.response import Response
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+
+        data = super().validate(attrs)
+        user = self.user
+        if user.is_active:
+            print(1)
+        return data
+
+
     @classmethod
     def get_token(cls, user):
-        token = super().get_token(user)
-        token['username'] = user.username 
-        token['mbti_type'] = user.mbti.mbti_type if user.mbti else None
-        return token
+        
+        if user.is_active:
+            token = super().get_token(user)
+            token['username'] = user.username 
+            token['mbti_type'] = user.mbti.mbti_type if user.mbti else None
+            return token
+        else: 
+            return Response({})
+
 
     @classmethod
     def refresh_token(cls, user):
+        
         token = super().get_token(user)
 
         # Refresh custom claims to the token payload
         token['username'] = user.username
         token['mbti_type'] = user.mbti.mbti_type if user.mbti else None
-
         return token
+    
+    
