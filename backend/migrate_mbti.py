880f985e80474dcff0f64ffd3b13ec89fe97905a
sang-il <lsi9393@naver.com>
2024-05-27 17:06:33 +0900

    feat 마이그레이션 * Comment에 mbti정보를 저장하도록 migration 적용

--- backend/migrate_mbti.py
+++ backend/migrate_mbti.py
@@ -8,22 +8,22 @@ from django.db import transaction
 
 
 def populate_post_mbti():
-    # 모든 Post 인스턴스에 대해 순회합니다.
     for post in Post.objects.all():
-        # 해당 Post의 작성자의 MBTI를 가져옵니다.
         author_mbti_type = post.author.mbti.mbti_type
-        # 해당 MBTI 유형에 해당하는 Mbti 인스턴스를 가져옵니다.
         post_mbti_instance = Mbti.objects.get(mbti_type=author_mbti_type)
-        # Post의 post_mbti 필드를 해당 MBTI 인스턴스로 업데이트합니다.
         post.post_mbti = post_mbti_instance
-        # 변경 사항을 저장합니다.
         post.save()
 
 
+def populate_comment_mbti():
+    for comment in Comment.objects.all():
+        author_mbti_type = comment.author.mbti.mbti_type
+        comment_mbti_instance = Mbti.objects.get(mbti_type=author_mbti_type)
+        comment.comment_mbti = comment_mbti_instance
+        comment.save()
+
 def main():
-    # 데이터베이스 작업을 트랜잭션으로 묶습니다.
     with transaction.atomic():
-        # populate_post_mbti 함수를 호출하여 post_mbti 필드를 채웁니다.
         populate_post_mbti()
 
 
