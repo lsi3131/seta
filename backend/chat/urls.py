63d24db89a912a1dd386ddb6862f76cc82de3265
sun-sunDayz <kill.yanggochi@gmail.com>
2024-05-31 19:39:45 +0900

    chatListPage

---
+++ backend/chat/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.ChatRoomAPIView.as_view(), name="chat_room"),
+    path("<int:chatroom_pk>/", views.ChatRoomAPIView.as_view(), name='chat_room_pk'),
+    path("<int:chatroom_pk>/message/", views.ChatMessageAPIView.as_view(), name='chat_message'),
+]
