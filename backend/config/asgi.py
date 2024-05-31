c31cfb5e43168ee7156b8de1944e9bc7dcaaad26
sang-il <lsi9393@naver.com>
2024-05-31 14:04:20 +0900

    * 채팅 routing 되도록 구현

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,9 +8,29 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
+# application = ProtocolTypeRouter({
+#     'http': get_asgi_application(),
+#     # (http->django views is added by default)
+#     # 만약에 websocket protocol 이라면, AuthMiddlewareStack
+#     'websocket': AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+#         # URLRouter 로 연결, 소비자의 라우트 연결 HTTP path를 조사
+#     ),
+# })
