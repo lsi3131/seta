7ca0cb5366cdb0245cdc800426e5f7b6eb7981c3
김병민 <gimbyeongmin@byeong.local>
2024-06-03 00:43:09 +0900

    기존의 chatting room components로 나누기

---
+++ frontend/src/Components/ChattingRoom/ChatRightTop.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useRef, useEffect } from "react";
+import style from "./ChattingRoom.module.css";
+
+
+const ChatRightTop = () => {
+
+
+    return (
+        <div className={style.Room_right_top}>
+            {/*  
+            1. 자물쇠 비밀방
+            2. 제목
+            3. 게임 정보?
+            */}
+        </div>
+    );
+};
+
+export default ChatRightTop;
