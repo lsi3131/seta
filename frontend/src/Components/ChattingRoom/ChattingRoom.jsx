397ca89891a2d319e8dc86c2cb5bed8b950bda33
sun-sunDayz <kill.yanggochi@gmail.com>
2024-06-03 14:21:15 +0900

    feature/chatEnterLeave

--- frontend/src/Components/ChattingRoom/ChattingRoom.jsx
+++ frontend/src/Components/ChattingRoom/ChattingRoom.jsx
@@ -1,24 +1,23 @@
-import React, { useState, useRef, useEffect } from "react";
-import style from "./ChattingRoom.module.css";
-import ChatRightBottom from "./ChatRightbottom";
-import ChatRightTop from "./ChatRightTop"
+import React, { useState, useRef, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import style from './ChattingRoom.module.css'
+import ChatRightBottom from './ChatRightbottom'
+import ChatRightTop from './ChatRightTop'
 
 const ChatRoom = () => {
-    
+    const { roomId } = useParams()
 
     return (
         <div className={style.Room_vertical}>
             <div className={style.Room_container}>
-                <div className={style.Room_left}>
-                    
-                </div>
+                <div className={style.Room_left}></div>
                 <div className={style.Room_right}>
                     <ChatRightTop />
-                    <ChatRightBottom />
+                    <ChatRightBottom roomId={roomId} />
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default ChatRoom;
+export default ChatRoom
